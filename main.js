7ca978874b0e01ceeaf01db5b26e3faa5b5937ea
Riccardo <riky.volto@live.com>
2023-08-30 15:23:22 +0200

    print solution in console log

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ Consigli:
 Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
  */
+
+
+let elementoSpesa;
+let listaSpesa = ['banane', 'yogurt', 'pollo' , 'bresaola', 'tonno' , 'tacchino']
+console.log(listaSpesa);
+let i = -1
+while (i < 5) {
+    i++
+    elementoSpesa = listaSpesa
+  console.log(elementoSpesa[i]);
+    
+}
